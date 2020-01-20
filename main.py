92db9f9224f47211a9c2a81e809a4af3222370cf
Vincent <lorg.vincent@gmail.com>
2020-01-20 23:00:37 +0100

    Ajout de la base de donné local

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ Script à lancer en premier
 
 """
 import subprocess
+import mysql.connector
 
 def run_script(script):
-    proc = subprocess.Popen(['bash', '-c', script], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    proc = subprocess.Popen(['bash', '-c', script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     if proc.returncode:
         raise print("Erreur script: " + stderr)
     return stdout
 
 retour = run_script('./script_bash')
-print(retour)
+
+#Recuperer la Date + Heure.
+date = retour[2:19]
+temp = retour[25:29]
+
+#Prepare la requete insertion
+inser = (date, temp)
+
+conn = mysql.connector.connect(host="localhost",user="admin",password="azerty", database="grafana_metrics")
+cursor = conn.cursor()
+cursor.execute(""" INSERT INTO `TEMPERATURE_CPU_CP` (`CP_TIME`, `CP_TEMP`) VALUES (%s, %s) """, inser)
+conn.commit() #Commit les modification
+conn.close() #Ferme la base
